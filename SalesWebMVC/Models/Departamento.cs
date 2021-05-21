2a685c2e10e3475cbbc9f5f2e74c945d2c3db67c
Lucas Castro <lukas.castro@live.com>
2021-05-21 15:15:51 -0300

    delete view w controle

--- SalesWebMVC/Models/Departamento.cs
+++ SalesWebMVC/Models/Departamento.cs
@@ -1,8 +1,13 @@
-﻿namespace SalesWebMVC.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
 {
     public class Departamento
     {
         public int Id { get; set; }
-        public string  Nome { get; set; }
+        public string Nome { get; set; }
     }
 }
